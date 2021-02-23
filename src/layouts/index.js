275d3000008eb64ac0ddc2f58c15c33f2465a4f8
Przemyslaw Mejna <pmejna@gmail.com>
2021-02-23 01:01:12 +0000

    re-build gatsby for netlify deployment

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import Header from '../components/Header/Header';
-import SideBar from '../components/SideBar/Sidebar';
+import SideBar from '../components/SideBar/SideBar';
 import GlobalStyles from '../assets/styles/GlobalStyles';
 import '../assets/styles/fonts/Index.css';
 
 const MainLayout = ({children}) => {
-    return (
+    return (    
     <>
         <GlobalStyles/>
         <Header/>
